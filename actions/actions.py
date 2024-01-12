8f01241e574e1f10d5994c2444a7837d294f1b79
thanhhuy <adjustmode1@gmail.com>
2024-01-12 16:52:54 +0700

    feat

--- actions/actions.py
+++ actions/actions.py
@@ -4,11 +4,15 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from rasa_core_sdk import Action
+# from rasa_core_sdk.events import SlotSet
+# from rasa_core_sdk.events import UserUtteranceReverted
+# from rasa_core_sdk.events import AllSlotsReset
+# from rasa_core_sdk.events import Restarted
 
-import requests
-import json
-from bs4 import BeautifulSoup
-from pyvi import ViTokenizer, ViPosTagger
+# import requests
+# import json
+# from bs4 import BeautifulSoup
+# from pyvi import ViTokenizer, ViPosTagger
 # import mysql.connector
 
 
